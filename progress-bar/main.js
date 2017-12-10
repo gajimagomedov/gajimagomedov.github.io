01792bd2ca9e3c48e8ab8bec1ff4d6452c4e8beb
gadjimagomedov <islam7581@mail.ru>
2017-12-11 01:56:17 +0300

    Updated progress-bar

--- progress-bar/main.js
+++ progress-bar/main.js
@@ -1,46 +1,80 @@
 (function(){
-	const btn = document.querySelector('button');
-	const progressBar = document.querySelector('.progress-bar');
-  const firstCircle = document.getElementById('circle-one');
-	const secondCircle = document.getElementById('circle-two');
-	let angle = 0;
-  let percent = 70*5.7;
 
-	let timer = setInterval(function(){
-    secondCircle.setAttribute("stroke-dasharray", angle + ", 20000");
-    firstCircle.innerHTML = parseInt(angle/471*100);
-    
-    if (angle >= percent) {
-      clearInterval(timer);
-    }
-    angle += 5;
-  }, 30);
-	
-	btn.onclick = () => {
-		//progress.style.transform = 'none';
-		progress.style.animation = 'preloader 3.5s infinite linear';
-	}
-	
-	let inputValue = document.querySelector('.progress-control__value');
-	
-	
-	function valueProgress(){
+	const inputValueProgress = document.getElementById("progress-value");
+	const btnStartAnimate = document.getElementById("checkbox-animate");
+	const btnHideProgress = document.getElementById("checkbox-hide");
+
+	function progressBarPrototye(){
+		const progressBar = document.querySelector(".progress-container");
+		const progressValue = progressBar.querySelector(".progress-bar__value");
+		const radius = progressValue.getAttribute("r");
+		let circumference = 2 * Math.PI * radius;
+
+		progressValue.style.strokeDasharray = circumference;
+
+		function setProgress(value) {
+			let progress = value / 100;
+			let dashoffset = circumference * (1 - progress);
+		
+			progressValue.style.strokeDashoffset = dashoffset;
+			animateValue(0, value, 1500);
+		}
+
+			
+		function animateValue(start, end, duration) {
+			let range = end - start;
+			let minTimer = 50;
+			let stepTime = Math.abs(Math.floor(duration / range));
+		
+			stepTime = Math.max(stepTime, minTimer);
+			
+			let startTime = new Date().getTime();
+			let endTime = startTime + duration;
+
+			function run() {
+				let now = new Date().getTime();
+				let remaining = Math.max((endTime - now) / duration, 0);
+				let value = Math.round(end - remaining * range);
+				if (value == end) {
+				clearInterval(timer);
+				}
+			}
+			
+			let timer = setInterval(run, stepTime);
+			run();
+		}
+
+		function progressAnimate(){
+			progressValue.classList.toggle("animate");
+		}
+
+		function hideProgressBar(){
+			progressBar.classList.toggle("hidden");
+		}
+
+		return { setProgress, progressAnimate, hideProgressBar };
 		
 	}
-	
-	inputValue.addEventListener('keydown', function(e){
-		  e = e || event;
 
-  if (e.ctrlKey || e.altKey || e.metaKey) return;
+	let progressBar = progressBarPrototye();
+	progressBar.setProgress(inputValueProgress.value);
 
 
-		if(e.keyCode === 38)
-			this.value < 100 ? this.value++ : false;
-		else if(e.keyCode === 40)
-			this.value == 0 ? false : this.value--;
-		
-		e.preventDefault();
+	inputValueProgress.addEventListener("input", function(e){
+		let value = +this.value;
+		value = isNaN(value) ? 0 : value;
+
+		if (value > 100 || value < 0) {
+			value = value > 100 ? 100 : 0;	
+		}
+
+		this.value = value;
+		progressBar.setProgress(value);
 	});
-	
-	
+
+
+
+	btnStartAnimate.addEventListener("click", progressBar.progressAnimate);
+	btnHideProgress.addEventListener("click", progressBar.hideProgressBar);
+
 })();
